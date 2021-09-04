b55d356d747a3c7e88ffdfbede20b2f559afce20
siva0410 <42541197+siva0410@users.noreply.github.com>
2021-09-04 08:40:15 -0700

    add abc215 d

--- abc215/d.cpp
+++ abc215/d.cpp
@@ -3,24 +3,45 @@
 #include <vector>
 #include <set>
 #include <map>
-
+#define SIZE 100005
 using namespace std;
 
 int main(){
   int n, m;
   cin >> n >> m;
-  vector<int> a(n);
+  vector<int> A(n), prime;
+  vector<bool> ng(SIZE, true);	 
   for(int i=0; i<n; i++){
-    cin >> a.at(i);
+    cin >> A.at(i);
   }
 
-  vector<bool> ng;
-  for(int i=0; i<n; i++){
-    for(int j=i+1; j<n; j+=i){
-      if(a.at(i)%
-	 
-    
-
+  for(auto a: A){
+    for(int i=2; i*i<=a; i++){
+      if(a%i != 0) continue;
+      while(a%i == 0){
+	a = a/i;
+      }
+      prime.push_back(i);
+    }
+    if(a!=1) prime.push_back(a);
+  }
+  
+  for(auto& p: prime){
+    if(ng.at(p)){
+      for(int i=p; i<SIZE; i+=p){
+	ng.at(i) = false;
+      }
+    }
+  }
+  vector<int> ans;
+  for(int i=1; i<=m; i++){
+    if(ng.at(i)) ans.push_back(i);
+  }
+  
+  cout << ans.size() << '\n';
+  for(auto& a: ans){
+    cout << a << '\n';
+  }
   
   return 0;
 }
