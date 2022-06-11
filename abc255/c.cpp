0b9e1c48a3477773c860729a4d16df6836021546
siva0410 <42541197+siva0410@users.noreply.github.com>
2022-06-12 01:08:59 +0900

    update c

--- abc255/c.cpp
+++ abc255/c.cpp
@@ -25,17 +25,24 @@ int main(){
   }
 
   long long left = 0, right = n-1, ans = 0;
-  while(left<=right){
-    long long mid = (left+right)/2;
-    // cout << left << " " << mid << " " << right << '\n';
-    if(a+d*mid <= x && x <= a+d*(mid+1)){
-      ans = min(x-(a+d*mid),a+d*(mid+1)-x);
-      break; 
-    }
-    if(a+d*(mid+1) > x) right = mid-1;
-    if(a+d*mid < x) left = mid+1;
-  }
+  // while(left<=right){
+  //   long long mid = (left+right)/2;
+  //   // cout << left << " " << mid << " " << right << '\n';
+  //   if(a+d*mid <= x && x <= a+d*(mid+1)){
+  //     ans = min(x-(a+d*mid),a+d*(mid+1)-x);
+  //     break; 
+  //   }
+  //   if(a+d*(mid+1) > x) right = mid-1;
+  //   if(a+d*mid < x) left = mid+1;
+  // }
 
+  long long mid = (left+right)/2;
+  while(right-left>1){
+    mid = (left+right)/2;
+    if(a+d*mid >= x) right = mid;
+    else left = mid;
+  }
+  ans = min(x-(a+d*mid), a+d*(mid+1)-x);
   cout << ans << endl;
   return 0;
 }
